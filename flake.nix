d2488092716326eee9466930d60a12ec380f0f66
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:37 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Sigstore common Go library for signing, verification, and OIDC Fulcio client";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      mkGoLibraryCheck = (import "${substrate}/lib/go-library-check.nix").mkGoLibraryCheck;
+    in {
+      checks.default = mkGoLibraryCheck pkgs {
+        pname = "sigstore";
+        version = "0.0.0-dev";
+        src = self;
+        vendorHash = "sha256-yCoSdz9HKf6rFWdv3Dyui4bHq6av6nrNIXqwpBpxCDA="; # COMPUTING
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ go gopls gotools ];
+      };
+    });
+}
